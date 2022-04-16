5e8a704a61d5da78d696dd7a30e23d96b3294827
Isaac <isaacray16@gmail.com>
2022-04-15 22:24:12 -0400

    Changes replaced round1, round2 and round3 variables with better variable names ie. roundTax, roundTotal and roundMonthlyPayments respectively

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner inputPrice = new Scanner(System.in);
         Scanner inputAcc = new Scanner(System.in);
+        Scanner inputTax = new Scanner(System.in);
+        System.out.println("What is your area sales tax in decimal form?");
+        double salesTax = Double.valueOf(inputTax.nextLine());
         System.out.println("What is the item's price?");
         double price = Double.valueOf(inputPrice.next());
         double accPrice = 0;
         int upgradeSupportCharge = 30;
-        double salesTax = 0.06;
         double totalTax = (accPrice + price) * salesTax;
         double total = price + totalTax;
         double monthlyPayments = price / 24;
-        double round1 = Math.round(totalTax * 100.00) / 100.00;
-        double round2 = Math.round(total * 100.00) / 100.00;
-        double round3 = Math.round(monthlyPayments * 100.00) / 100.00;
-        double totalToday = round1 + upgradeSupportCharge;
+        double roundTax = Math.round(totalTax * 100.00) / 100.00;
+        double roundTotal = Math.round(total * 100.00) / 100.00;
+        double roundMonthlyPayments = Math.round(monthlyPayments * 100.00) / 100.00;
+        double totalToday = roundTax + upgradeSupportCharge;
         if (price <= 200.00) {
             System.out.println("The price of the accessories is: " + "$" + price);
-            System.out.println("The sales tax is: " + "$" + round1);
-            System.out.println("Your total is: " + "$" + round2);
+            System.out.println("The sales tax is: " + "$" + roundTax);
+            System.out.println("Your total is: " + "$" + roundTotal);
             System.exit(0);
         }
         System.out.println("Are there accessories?");
@@ -32,19 +34,19 @@ public class Main {
             accPrice = Double.parseDouble(inputAccPrice.nextLine());
             System.out.println("The price of the item is: " + "$" + price);
             System.out.println("The price of the accessories is: " + "$" + accPrice);
-            System.out.println("The sales tax is: " + "$" + round1);
+            System.out.println("The sales tax is: " + "$" + roundTax);
             System.out.println("Upgrade Support Charge: " + "$" + upgradeSupportCharge);
             System.out.println("Your total today is " + "$" + (totalToday + accPrice));
-            System.out.println("Your total is: " + "$" + (round2 + upgradeSupportCharge));
-            System.out.println("Your monthly payment is " + "$" + round3);
+            System.out.println("Your total is: " + "$" + (roundTotal + upgradeSupportCharge));
+            System.out.println("Your monthly payment is " + "$" + roundMonthlyPayments);
         }
         else {
             System.out.println("The price of the item is: " + "$" + price);
-            System.out.println("The sales tax is: " + "$" + round1);
+            System.out.println("The sales tax is: " + "$" + roundTax);
             System.out.println("Upgrade Support Charge: " + "$" + upgradeSupportCharge);
             System.out.println("Your total today is " + "$" + totalToday);
-            System.out.println("Your total is: " + "$" + (round2 + upgradeSupportCharge));
-            System.out.println("Your monthly payment is " + "$" + round3);
+            System.out.println("Your total is: " + "$" + (roundTotal + upgradeSupportCharge));
+            System.out.println("Your monthly payment is " + "$" + roundMonthlyPayments);
         }
 
     }
